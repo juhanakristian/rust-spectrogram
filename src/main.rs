b77cd0a26743bba3084d59b0341c81429b2a293e
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-11-09 23:34:52 +0200

    Started implementing fft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use num::complex::Complex;
+use std;
+use std::f32;
+
+fn fft(buffer: Vec<u16>) -> Vec<u16> {
+    let N = buffer.len();
+    if N == 1 {
+        return buffer;
+    }
+
+    let r: f32 = -2.00 * f32::consts::PI / (N as f32);
+    let root = Complex::new(r.cos(), r.sin());
+
+    let even: &mut Vec<Complex<f32>> = &mut vec![];
+    let odd: &mut Vec<Complex<f32>> = &mut vec![];
+
+    return vec![0];
+}
+
 fn main() {
     println!("Hello, world!");
 }
