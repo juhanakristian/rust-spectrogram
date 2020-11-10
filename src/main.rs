91ca02b6f05f5133b121d07931ab44c2c8b05f3e
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-11-10 22:44:11 +0200

    Copied fft from Skrolli

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use num::complex::Complex;
 use std;
 use std::f32;
 
-fn fft(buffer: Vec<u16>) -> Vec<u16> {
+fn fft(buffer: &mut Vec<Complex<f32>>) -> &mut Vec<Complex<f32>> {
     let N = buffer.len();
     if N == 1 {
         return buffer;
@@ -11,10 +11,28 @@ fn fft(buffer: Vec<u16>) -> Vec<u16> {
     let r: f32 = -2.00 * f32::consts::PI / (N as f32);
     let root = Complex::new(r.cos(), r.sin());
 
-    let even: &mut Vec<Complex<f32>> = &mut vec![];
-    let odd: &mut Vec<Complex<f32>> = &mut vec![];
+    let p_even = &mut vec![];
+    let p_odd = &mut vec![];
 
-    return vec![0];
+    for i in (0..N).step_by(2) {
+        p_even.push(buffer[i]);
+    }
+
+    for i in (1..N).step_by(2) {
+        p_odd.push(buffer[i]);
+    }
+
+    let even = fft(p_even);
+    let odd = fft(p_odd);
+
+    let mut r: Complex<f32> = Complex::new(1.00, 0.00);
+    for i in 0..N / 2 {
+        buffer[i] = even[i] + r * odd[i];
+        buffer[N / 2 + i] = even[i] - r * odd[i];
+        r *= root;
+    }
+
+    return buffer;
 }
 
 fn main() {
