ee58be8d8f04b88f9c43d75b9a8d3416138ad6ef
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-11-15 23:36:08 +0200

    Added dft from Skrolli

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,21 @@ fn fft(buffer: &mut Vec<Complex<f32>>) -> &mut Vec<Complex<f32>> {
     return buffer;
 }
 
+fn dft(input: &mut Vec<f32>) -> Vec<Complex<f32>> {
+    let n = input.len();
+    let i = Complex::new(0.0, 1.0);
+    let mut ans: Vec<Complex<f32>> = vec![Complex::new(0.0, 0.0); n];
+    for k in 0..n {
+        for j in 0..n {
+            let kf32 = k as f32;
+            let jf32 = j as f32;
+            let nf32 = n as f32;
+            ans[k] += input[j] * (-2.0 * f32::consts::PI * kf32 * jf32 / nf32 * i).exp();
+        }
+    }
+    return ans;
+}
+
 fn main() {
     println!("Hello, world!");
 }
